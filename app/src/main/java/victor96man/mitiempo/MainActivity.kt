5e97dee3caf46d771cc077ba08a098cb4af4fea2
Víctor M Gomez <34237832+Victor96Man@users.noreply.github.com>
2019-01-24 10:43:21 +0100

    Initial commit

---
+++ app/src/main/java/victor96man/mitiempo/MainActivity.kt
@@ -0,0 +1,12 @@
+package victor96man.mitiempo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
